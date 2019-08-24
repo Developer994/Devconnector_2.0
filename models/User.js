93473c99c8109a90fbe15b33d112b2d0ff4bc8bc
Silent Dev <rez_a@outlook.com>
2019-08-24 16:34:57 -0400

    Created the api folder under routes containing the routes for auth, posts, profile and users. Also made changes in the server file by add the app.use for all 4 of the routes.

---
+++ models/User.js
@@ -0,0 +1,27 @@
+const mongooose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    avatar: {
+        type: String,
+
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+modules.exports = User = mongooose.model('user', User)
