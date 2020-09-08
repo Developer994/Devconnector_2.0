c62e2a381d3bd97c83e7239a0b44d87b5573eff0
Silent Dev <rez_a@outlook.com>
2020-09-08 16:31:47 -0400

    Took care of some of the Console Warnings by removing unused defined components/methods

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -10,7 +10,6 @@ import {
   ACCOUNT_DELETED,
   GET_REPOS,
 } from "./types";
-import { Redirect } from "react-router-dom";
 
 // Get current users profile
 export const getCurrentProfile = () => async (dispatch) => {
@@ -227,7 +226,7 @@ export const deleteEducation = (id) => async (dispatch) => {
 export const deleteAccount = (id) => async (dispatch) => {
   if (window.confirm("Are you sure? This can NOT be undone!")) {
     try {
-      const res = await axios.delete(`/api/profile`);
+      await axios.delete(`/api/profile`);
 
       dispatch({
         type: CLEAR_PROFILE,
