3fd40d18a62b8bd79791c95d62c3a06e3d6510f2
Silent Dev <rez_a@outlook.com>
2020-09-15 12:36:11 -0400

    Fixed the mistake in server.js ( /api/Auth to /api/auth and same for the require)

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.get("/", (req, res) => res.send("API Running"));
 
 // Define routes
 app.use("/api/users", require("./routes/api/users"));
-app.use("/api/Auth", require("./routes/api/Auth"));
+app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/posts", require("./routes/api/posts"));
 
